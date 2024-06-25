3305933a956993393727a7622c4219d5b1325566
Matheus Alex Silva Gouvea <matheusalexusb@gmail.com>
2024-06-25 12:36:56 -0300

    25_06_2024_commit_2

--- src/main/java/com/matheusgouvea/lpcounter_yu_gi_oh/MainActivity.java
+++ src/main/java/com/matheusgouvea/lpcounter_yu_gi_oh/MainActivity.java
@@ -15,15 +15,12 @@ import android.view.ViewGroup;
 import android.text.InputType;
 import android.widget.EditText;
 import android.content.DialogInterface;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 
 import com.matheusgouvea.lpcounter_yu_gi_oh.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'lpcounter_yu_gi_oh' library on application startup.
-    static {
-        System.loadLibrary("lpcounter_yu_gi_oh");
-    }
 
     private ActivityMainBinding binding;
 
@@ -34,18 +31,15 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Set up the TextView for displaying the prompt
         TextView textView = findViewById(R.id.textView);
         textView.setText(R.string.select_player_number);
 
-        // Set up the spinner for selecting the number of players
         Spinner playerSpinner = findViewById(R.id.player_spinner);
-        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_item) {
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item) {
             @Override
             public View getDropDownView(int position, View convertView, ViewGroup parent) {
                 View view = super.getDropDownView(position, convertView, parent);
                 TextView textView = (TextView) view;
-                // Set the text color for the dropdown items to white
                 textView.setTextColor(Color.BLACK);
                 return view;
             }
@@ -54,36 +48,26 @@ public class MainActivity extends AppCompatActivity {
             public View getView(int position, View convertView, ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
                 TextView textView = (TextView) view;
-                // Keep the text color for the selected item as white
                 textView.setTextColor(Color.WHITE);
                 return view;
             }
         };
 
-        Spinner spinner = (Spinner) findViewById(R.id.player_spinner);
-        spinner.setAdapter(adapter);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         adapter.addAll(getResources().getStringArray(R.array.player_numbers));
         playerSpinner.setAdapter(adapter);
 
-        // Set up the button for confirming the selection
         Button confirmButton = findViewById(R.id.confirm_button);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Get the selected number of players
                 int numPlayers = Integer.parseInt(playerSpinner.getSelectedItem().toString());
-
-                // Create an array to hold the player names
                 String[] playerNames = new String[numPlayers];
-
-                // Start the process of creating dialogs for each player
                 createPlayerDialog(playerNames, 0, numPlayers);
             }
         });
     }
 
     private void createPlayerDialog(String[] playerNames, int playerIndex, int numPlayers) {
-        // Create a dialog to input the player name
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("Enter the name of player " + (playerIndex + 1));
 
@@ -91,34 +75,59 @@ public class MainActivity extends AppCompatActivity {
         input.setInputType(InputType.TYPE_CLASS_TEXT);
         builder.setView(input);
 
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("OK", null); // Set null here to override later
+
+        final AlertDialog dialog = builder.create();
+
+        // Listener para detectar o clique no botão "Confirmar" do teclado virtual
+        input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                playerNames[playerIndex] = input.getText().toString();
-
-                // If this is the last player, start the new activity
-                if (playerIndex == numPlayers - 1) {
-                    // Display a message
-                    Toast.makeText(MainActivity.this, "Selected " + numPlayers + " players", Toast.LENGTH_SHORT).show();
-
-                    // Start the LP Calculator Activity and pass the number of players and the player names
-                    Intent intent = new Intent(MainActivity.this, LpCalculatorActivity.class);
-                    intent.putExtra("numPlayers", numPlayers);
-                    intent.putExtra("playerNames", playerNames);
-                    startActivity(intent);
-                } else {
-                    // Otherwise, create the next dialog
-                    createPlayerDialog(playerNames, playerIndex + 1, numPlayers);
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    // Simula o clique no botão "OK" do diálogo
+                    playerNames[playerIndex] = input.getText().toString();
+                    if (playerIndex == numPlayers - 1) {
+                        Toast.makeText(MainActivity.this, "Selected " + numPlayers + " players", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, LpCalculatorActivity.class);
+                        intent.putExtra("numPlayers", numPlayers);
+                        intent.putExtra("playerNames", playerNames);
+                        startActivity(intent);
+                    } else {
+                        createPlayerDialog(playerNames, playerIndex + 1, numPlayers);
+                    }
+                    dialog.dismiss();
+                    return true; // Consumir o evento aqui
                 }
+                return false; // Passar o evento para o próximo listener
             }
         });
 
-        builder.show();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                Button button = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Ação do botão "OK" já definida acima
+                        playerNames[playerIndex] = input.getText().toString();
+                        if (playerIndex == numPlayers - 1) {
+                            Toast.makeText(MainActivity.this, "Selected " + numPlayers + " players", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, LpCalculatorActivity.class);
+                            intent.putExtra("numPlayers", numPlayers);
+                            intent.putExtra("playerNames", playerNames);
+                            startActivity(intent);
+                        } else {
+                            createPlayerDialog(playerNames, playerIndex + 1, numPlayers);
+                        }
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        dialog.show();
     }
 
-    /**
-     * A native method that is implemented by the 'lpcounter_yu_gi_oh' native library,
-     * which is packaged with this application.
-     */
     public native String stringFromJNI();
 }
