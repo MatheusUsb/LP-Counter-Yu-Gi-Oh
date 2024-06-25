a210ba7e4ecd8093d1fd698dfb6059da06aca36f
Matheus Alex Silva Gouvea <matheusalexusb@gmail.com>
2024-06-25 09:33:01 -0300

    25_06_2024_commit_1

--- src/main/java/com/matheusgouvea/lpcounter_yu_gi_oh/LpCalculatorActivity.java
+++ src/main/java/com/matheusgouvea/lpcounter_yu_gi_oh/LpCalculatorActivity.java
@@ -2,14 +2,15 @@ package com.matheusgouvea.lpcounter_yu_gi_oh;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.widget.TextView;
-import android.widget.Button;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.text.InputType;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.inputmethod.EditorInfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LpCalculatorActivity extends AppCompatActivity {
@@ -34,99 +35,64 @@ public class LpCalculatorActivity extends AppCompatActivity {
         playerNames = getIntent().getStringArrayExtra("playerNames");
 
         // Initialize the arrays
-        lifePoints = new int[4];
-        lifePointViews = new TextView[4];
-        playerNameViews = new TextView[4]; // Initialize the array for the player name views
-        addButtons = new Button[4];
-        subtractButtons = new Button[4];
+        lifePoints = new int[numPlayers];
+        lifePointViews = new TextView[numPlayers];
+        playerNameViews = new TextView[numPlayers]; // Initialize the array for the player name views
+        addButtons = new Button[numPlayers];
+        subtractButtons = new Button[numPlayers];
 
-        for (int i = 0; i < 4; i++) {
-            if (i < numPlayers) {
-                final int finalI = i;
-                lifePoints[i] = 8000;
-
-                // Get the TextView for the player name and set the player name
-                playerNameViews[i] = findViewById(getResources().getIdentifier("player" + (i+1) + "Name", "id", getPackageName()));
-                playerNameViews[i].setText(playerNames[i]);
-
-                lifePointViews[i] = findViewById(getResources().getIdentifier("player" + (i+1) + "LifePoints", "id", getPackageName()));
-                updateLifePoints(i);
-
-                addButtons[i] = findViewById(getResources().getIdentifier("player" + (i+1) + "AddButton", "id", getPackageName()));
-                addButtons[i].setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v) {
-                        openDialog(true, finalI);
-                    }
-                });
-
-                subtractButtons[i] = findViewById(getResources().getIdentifier("player" + (i+1) + "SubtractButton", "id", getPackageName()));
-                subtractButtons[i].setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v) {
-                        openDialog(false, finalI);
-                    }
-                });
-
-                playerNameViews[i].setVisibility(View.VISIBLE);
-                lifePointViews[i].setVisibility(View.VISIBLE);
-                addButtons[i].setVisibility(View.VISIBLE);
-                subtractButtons[i].setVisibility(View.VISIBLE);
-            } else {
-                playerNameViews[i] = findViewById(getResources().getIdentifier("player" + (i+1) + "Name", "id", getPackageName()));
-                lifePointViews[i] = findViewById(getResources().getIdentifier("player" + (i+1) + "LifePoints", "id", getPackageName()));
-                addButtons[i] = findViewById(getResources().getIdentifier("player" + (i+1) + "AddButton", "id", getPackageName()));
-                subtractButtons[i] = findViewById(getResources().getIdentifier("player" + (i+1) + "SubtractButton", "id", getPackageName()));
-
-                playerNameViews[i].setVisibility(View.GONE);
-                lifePointViews[i].setVisibility(View.GONE);
-                addButtons[i].setVisibility(View.GONE);
-                subtractButtons[i].setVisibility(View.GONE);
-            }
-        }
         for (int i = 0; i < numPlayers; i++) {
+            final int finalI = i;
+            lifePoints[i] = 8000;
+
+            // Get the TextView for the player name and set the player name
+            playerNameViews[i] = findViewById(getResources().getIdentifier("player" + (i + 1) + "Name", "id", getPackageName()));
+            playerNameViews[i].setText(playerNames[i]);
+
+            lifePointViews[i] = findViewById(getResources().getIdentifier("player" + (i + 1) + "LifePoints", "id", getPackageName()));
+            updateLifePoints(i);
+
+            addButtons[i] = findViewById(getResources().getIdentifier("player" + (i + 1) + "AddButton", "id", getPackageName()));
+            addButtons[i].setOnClickListener(v -> openDialog(true, finalI));
+
+            subtractButtons[i] = findViewById(getResources().getIdentifier("player" + (i + 1) + "SubtractButton", "id", getPackageName()));
+            subtractButtons[i].setOnClickListener(v -> openDialog(false, finalI));
+
             playerNameViews[i].setVisibility(View.VISIBLE);
             lifePointViews[i].setVisibility(View.VISIBLE);
             addButtons[i].setVisibility(View.VISIBLE);
             subtractButtons[i].setVisibility(View.VISIBLE);
         }
 
-        for (int i = numPlayers; i < 4; i++) {
-            if(playerNameViews[i] != null) playerNameViews[i].setVisibility(View.GONE);
-            if(lifePointViews[i] != null) lifePointViews[i].setVisibility(View.GONE);
-            if(addButtons[i] != null) addButtons[i].setVisibility(View.GONE);
-            if(subtractButtons[i] != null) subtractButtons[i].setVisibility(View.GONE);
+        // Adjust visibility based on the number of players
+        for (int i = numPlayers; i < 4; i++) { // Assuming a maximum of 4 players
+            findViewById(getResources().getIdentifier("player" + (i + 1) + "Name", "id", getPackageName())).setVisibility(View.GONE);
+            findViewById(getResources().getIdentifier("player" + (i + 1) + "LifePoints", "id", getPackageName())).setVisibility(View.GONE);
+            findViewById(getResources().getIdentifier("player" + (i + 1) + "AddButton", "id", getPackageName())).setVisibility(View.GONE);
+            findViewById(getResources().getIdentifier("player" + (i + 1) + "SubtractButton", "id", getPackageName())).setVisibility(View.GONE);
         }
 
-        // Initialize the timer view
+        // Initialize the timer view and buttons
         timerView = findViewById(R.id.timerView);
-
-        // Initialize the timer buttons
         startPauseButton = findViewById(R.id.startPauseButton);
         resetButton = findViewById(R.id.resetButton);
 
-        startPauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isPaused) {
-                    if (countDownTimer == null) {
-                        startTimer();
-                        startPauseButton.setText("Pause");
-                    } else {
-                        resumeTimer();
-                        startPauseButton.setText("Pause");
-                    }
+        startPauseButton.setOnClickListener(v -> {
+            if (isPaused) {
+                if (countDownTimer == null) {
+                    startTimer();
+                    startPauseButton.setText("Pause");
                 } else {
-                    pauseTimer();
-                    startPauseButton.setText("Resume");
+                    resumeTimer();
+                    startPauseButton.setText("Pause");
                 }
+            } else {
+                pauseTimer();
+                startPauseButton.setText("Resume");
             }
         });
 
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetTimer();
-            }
-        });
+        resetButton.setOnClickListener(v -> resetTimer());
     }
 
     private void startTimer() {
@@ -148,11 +114,12 @@ public class LpCalculatorActivity extends AppCompatActivity {
     private void pauseTimer() {
         if (countDownTimer != null) {
             countDownTimer.cancel();
-            countDownTimer = null; // Add this line
+            countDownTimer = null;
         }
 
         isPaused = true;
     }
+
     private void resumeTimer() {
         startTimer();
     }
@@ -184,30 +151,33 @@ public class LpCalculatorActivity extends AppCompatActivity {
         input.setInputType(InputType.TYPE_CLASS_NUMBER);
         builder.setView(input);
 
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String inputValue = input.getText().toString();
-                if (!inputValue.isEmpty()) {
-                    int value = Integer.parseInt(inputValue);
-                    if (isAdding) {
-                        addLifePoints(playerIndex, value);
-                    } else {
-                        subtractLifePoints(playerIndex, value);
-                    }
-                } else {
-                    Toast.makeText(LpCalculatorActivity.this, "Please, insert a value", Toast.LENGTH_SHORT).show();
-                }
+        builder.setPositiveButton("OK", (dialog, which) -> processInputValue(input.getText().toString(), isAdding, playerIndex));
+        builder.setNegativeButton("Cancelar", (dialog, which) -> dialog.cancel());
+
+        AlertDialog dialog = builder.create();
+
+        input.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).performClick();
+                return true;
             }
-        });
-        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
+            return false;
         });
 
-        builder.show();
+        dialog.show();
+    }
+
+    private void processInputValue(String inputValue, boolean isAdding, int playerIndex) {
+        if (!inputValue.isEmpty()) {
+            int value = Integer.parseInt(inputValue);
+            if (isAdding) {
+                addLifePoints(playerIndex, value);
+            } else {
+                subtractLifePoints(playerIndex, value);
+            }
+        } else {
+            Toast.makeText(LpCalculatorActivity.this, "Please, insert a value", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void updateLifePoints(int playerIndex) {
